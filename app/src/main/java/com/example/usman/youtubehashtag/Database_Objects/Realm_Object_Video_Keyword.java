ba32d09820b8698d0c86c4607b2f801d11228792
usman-wearhealth <usmangeo14@gmail.com>
2017-05-13 12:44:22 -0700

    Commit One

---
+++ app/src/main/java/com/example/usman/youtubehashtag/Database_Objects/Realm_Object_Video_Keyword.java
@@ -0,0 +1,30 @@
+package com.example.usman.youtubehashtag.Database_Objects;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by usman on 3/16/2017.
+ */
+
+public class Realm_Object_Video_Keyword extends RealmObject{
+    String keyword;
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    int    key;
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+
+}
